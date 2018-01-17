baa5f524b707a7365ee5271f40a2cec5f6bfc505
Alex Gnedash <alex.gned@ya.ru>
2018-01-17 12:32:18 +0300

    ...

--- Task8/Task8.pl
+++ Task8/Task8.pl
@@ -1,9 +1,20 @@
-man(Y) :- Y=ivan; Y=petr; Y=andrew; Y=vasily.
-woman(X) :- X=anna; X=julia; X=maria; X=tatyana.
-condition(X,Y) :- (not(X=anna), not(Y=ivan)) -> (X=julia, Y=ivan).
-condition(X,Y) :- (X=anna , Y=petr) -> (not(X=julia), not(Y=ivan)).
-condition(X,Y) :- (not(X=maria), not(Y=vasily)) -> (X=julia,Y=andrew).
-condition(X,Y) :- (X=julia, Y=petr) -> (X=tatyana, Y=ivan).
-condition(X,Y) :- (not(X=maria), not(Y=petr)) -> (not(X=tatyana), not(Y=ivan)).
-condition(X,Y) :- (X=julia, Y=andrew) -> (X=maria, Y=ivan).
-% ?- condition(X,Y).
+c1(Ivan) :- not(Ivan = anna), !, Ivan = julia.
+c1(Ivan).
+c2(Peter, Ivan) :- Peter = anna, !, not(Ivan = julia).
+c2(Peter, Ivan).
+c3(Vasiliy, Andrew) :- not(Vasiliy = mary), !, Andrew=julia.
+c3(Vasiliy, Andrew).
+c4(Peter, Ivan) :- Peter = julia, !, Ivan=tatyana.
+c4(Peter, Ivan).
+c5(Peter, Ivan) :- not(Peter = mary), !, not(Ivan = tatyana).
+c5(Peter, Ivan).
+c6(Andrew, Ivan) :- Andrew = julia, !, Ivan = mary.
+c6(Andrew, Ivan).
+del(X, [H|T], [H|S]) :- X \= H, del(X, T, S).
+del(X, [X|T], T).
+eq([H|T],L) :- member(H, L), del(H, L, R), eq(T, R).
+eq([],[]).
+family(X) :- X=[[ivan,Ivan],[peter,Peter],[vasiliy,Vasiliy],[andrew,Andrew]], eq([Ivan,Peter,Vasiliy,Andrew],[mary,julia,anna,tatyana]), c1(Ivan), c2(Peter,Ivan), c3(Vasiliy,Andrew), c4(Peter,Ivan), c5(Peter,Ivan), c6(Andrew,Ivan).
+% ?-family(X).
+% Result: X = [[ivan, julia], [peter, tatyana], [vasiliy, mary], [andrew, anna]].
+% Проверял на https://swish.swi-prolog.org/
